e47c57bb24aa9dd3608fe728904e8d2a7206b55f
edaseigo <e1b24014@oit.ac.jp>
2025-10-09 22:07:41 +0900

    2025/10/09 江田 小さめのステージを作成

---
+++ 2025Kita/Assets/Scripts/UI/Scene.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene : MonoBehaviour
+{
+    public void ChangeMain()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void ChangeTitle()
+    {
+        SceneManager.LoadScene("Title");
+    }
+}
