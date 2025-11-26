ea12283b5b760f8db1b6b8b6e678bbf7a26fd5b4
LAPTOP-TTDPT3KH\ssaku <ssakuraa3@gmail.com>
2025-11-26 16:46:45 +0900

    2025/11/26 森本 カメラ調整

--- 2025Kita/Assets/Scripts/CameraChange.cs
+++ 2025Kita/Assets/Scripts/CameraChange.cs
@@ -5,6 +5,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Cinemachine;
 
 public class CameraChange : MonoBehaviour
 {
@@ -13,11 +14,13 @@ public class CameraChange : MonoBehaviour
     public PlayerMovement[] targetScripts;  // プレイヤー移動を管理するスクリプト
     public PlayerMovement currentScript;   // 現在動いているプレイヤーのスクリプト
     private int EggSelect = 0;
-    public int Changenum;
+    //public int Changenum;
 
     // Start is called before the first frame update
     void Start()
     {
+        int startIndex = Mathf.Clamp(EggSelect, 0, targetScripts.Length - 1);
+
         if(currentScript == null && targetScripts.Length > 0)
         {
             currentScript = targetScripts[0];
@@ -26,7 +29,14 @@ public class CameraChange : MonoBehaviour
         for (int i = 0; i < cameras.Length; i++)
         {
             // 使用中のスクリプト以外は非アクティブにしておく
-            cameras[i].SetActive(true); 
+            //cameras[i].SetActive(true); 
+            CinemachineVirtualCamera vcam = cameras[i].GetComponent<CinemachineVirtualCamera>();
+
+            if (vcam != null)
+            {
+                // 初期キャラのVCamのみ Priority を高く設定 (例: 15)
+                vcam.Priority = (i == startIndex) ? 15 : 5;
+            }
 
             if (targetScripts.Length > i && targetScripts[i] != null)
             {
@@ -34,9 +44,14 @@ public class CameraChange : MonoBehaviour
             }
         }
 
-        SubscribeToPlayerCollision(currentScript);
+        if (currentScript != null)
+        {
+            SubscribeToPlayerCollision(currentScript);
+        }
+
+        /*SubscribeToPlayerCollision(currentScript);
         currentScript.enabled = true;               // 現在使用しているオブジェクトのスクリプトをアクティブ状態にする
-        mainCamera.SetActive(true);                 // 現在使用しているカメラをアクティブ状態にする
+        mainCamera.SetActive(true);                 // 現在使用しているカメラをアクティブ状態にする*/
     }
 
     private void Update()
@@ -69,18 +84,35 @@ public class CameraChange : MonoBehaviour
 
             currentScript.SetMoveStop(true);
 
-            if(mainCamera != null)
+            // 現在のVCamの優先度を下げる (5)
+            GameObject oldCameraObject = cameras[Array.IndexOf(targetScripts, currentScript)];
+            CinemachineVirtualCamera oldVcam = oldCameraObject.GetComponent<CinemachineVirtualCamera>();
+
+            if (oldVcam != null)
+            {
+                oldVcam.Priority = 5;
+            }
+
+            /*if (mainCamera != null)
             {
                 mainCamera.SetActive(false);
-            }
+            }*/
         }
 
         if (num < cameras.Length && num < targetScripts.Length)
         {
-            mainCamera = cameras[num];
+            //mainCamera = cameras[num];
             currentScript = targetScripts[num];
 
-            mainCamera.SetActive(true);
+            // 新しいVCamの優先度を上げる (15)
+            GameObject newCameraObject = cameras[num];
+            CinemachineVirtualCamera newVcam = newCameraObject.GetComponent<CinemachineVirtualCamera>();
+
+            if (newVcam != null)
+            {
+                newVcam.Priority = 15;
+            }
+            //mainCamera.SetActive(true);
             currentScript.SetMoveStop(false);
             SubscribeToPlayerCollision(currentScript);
          }
@@ -93,8 +125,8 @@ public class CameraChange : MonoBehaviour
         {
             collodedObject.SetActive(false);
         }
-
-        EggChange(Changenum);
+        EggSelect = (EggSelect + 1) % targetScripts.Length;
+        EggChange(EggSelect);
         EggSelect++;
         
     }
